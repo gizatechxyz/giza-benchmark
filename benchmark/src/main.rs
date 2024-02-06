e827261d3623ae92dae52eb7bcefb1974e8fa2fd
raphaelDkhn <raphael@gizatech.xyz>
2024-02-06 15:56:48 +0200

    print metric tables

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,8 +1,12 @@
 use std::fs;
 
 use clap::{App, Arg};
-use tokio; // Ensure tokio is included for the async runtime
+use data::Metrics;
+use tokio;
 
+use crate::consts::BENCHMARK_TITLE; // Ensure tokio is included for the async runtime
+
+mod consts;
 mod data;
 mod orion;
 mod utils;
@@ -13,42 +17,62 @@ async fn main() {
         .version("1.0")
         .author("Your Name <your_email@example.com>")
         .about("Benchmarks Orion performance")
-        .arg(Arg::with_name("sierra_file")
-            .short('s')
-            .long("sierra-file")
-            .value_name("FILE")
-            .help("Sets the path to the Sierra file")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("args_file")
-            .short('a') 
-            .long("args-file")
-            .value_name("FILE")
-            .help("Sets the path to the file containing program arguments")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("benchmark_path")
-            .short('b')
-            .long("benchmark-path")
-            .value_name("PATH")
-            .help("Sets the path for benchmark outputs")
-            .takes_value(true)
-            .required(true))
+        .arg(
+            Arg::with_name("sierra_file")
+                .short('s')
+                .long("sierra-file")
+                .value_name("FILE")
+                .help("Sets the path to the Sierra file")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("args_file")
+                .short('a')
+                .long("args-file")
+                .value_name("FILE")
+                .help("Sets the path to the file containing program arguments")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("benchmark_path")
+                .short('b')
+                .long("benchmark-path")
+                .value_name("PATH")
+                .help("Sets the path for benchmark outputs")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let sierra_file = matches.value_of("sierra_file").unwrap().to_string();
     let args_file = matches.value_of("args_file").unwrap(); // Changed to match the new argument
     let benchmark_path = matches.value_of("benchmark_path").unwrap().to_string();
 
-    // Read the program arguments from the specified file
-    let program_args = fs::read_to_string(args_file)
-        .expect("Failed to read the program arguments file");
+    let program_args =
+        fs::read_to_string(args_file).expect("Failed to read the program arguments file");
 
-    let _benchmark = orion::benchmark_orion(
-        &sierra_file,
-        &program_args,
-        &benchmark_path,
-    ).await;
+    let benchmark = orion::benchmark_orion(&sierra_file, &program_args, &benchmark_path).await;
 
-    println!("Benchmark completed successfully.");
+    print_metrics_table(&benchmark.runner, &benchmark.prover, &benchmark.verifier);
+}
+
+fn print_metrics_table(runner: &Metrics, prover: &Metrics, verifier: &Metrics) {
+    println!("\n");
+    println!("{}", BENCHMARK_TITLE);
+    println!("\n");
+
+    println!("| Type               | Run          | Prove         | Verify       |");
+    println!("| ------------------ | ------------ | ------------- | ------------ |");
+    println!(
+        "| time (ms)          | {:13.6} | {:13.6} | {:13.6} |",
+        runner.exec_time * 1000.0,   // Convert seconds to ms
+        prover.exec_time * 1000.0,   // Convert seconds to ms
+        verifier.exec_time * 1000.0  // Convert seconds to ms
+    );
+    println!(
+        "| memory usage (KB)  | {:12.6} | {:13.6} | {:12.6} |",
+        runner.memory_usage, prover.memory_usage, verifier.memory_usage
+    );
 }
